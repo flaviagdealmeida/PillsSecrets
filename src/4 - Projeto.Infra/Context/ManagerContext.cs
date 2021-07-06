545098569ae28c0bcaa76a7f80369b7af9a67c17
Flavia Goncalves de Almeida <eufga1@gmail.com>
2021-07-06 19:37:58 -0300

    Add_Security

--- src/4 - Projeto.Infra/Context/ManagerContext.cs
+++ src/4 - Projeto.Infra/Context/ManagerContext.cs
@@ -8,7 +8,7 @@ namespace Projeto.Infra.Context{
         public ManagerContext(DbContextOptions <ManagerContext> options) : base(options) { }
 
         public override void OnConfiguring(DbContextOptionsBuilder optionsbuilder){
-           
+           optionsbuilder.UseSqlServer(@"Data Source=SQL5018.site4now.net;Initial Catalog=DB_A49C81_Banco;User Id=DB_A49C81_Banco_admin;Password=FGA12345678;");
         }
         public virtual DbSet<User> Users { get; set; }
 
